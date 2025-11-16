4d32a7e7f42ec400671d7d4199f58edc5554abe2
Muskanbalaiwar <125793580+Muskanbalaiwar@users.noreply.github.com>
2025-11-16 21:45:02 +0530

    db config changes

--- util/database.js
+++ util/database.js
@@ -1,8 +1,25 @@
-const Sequelize =require('sequelize');
+// const Sequelize =require('sequelize');
 
-const sequelize=new Sequelize(process.env.DB_DBNAME,process.env.DB_ROLE,process.env.DB_PASSWORD,{
-    dialect:process.env.DIALECT,
-    host:process.env.DB_HOST
-});
+// const sequelize=new Sequelize(process.env.DB_DBNAME,process.env.DB_ROLE,process.env.DB_PASSWORD,{
+//     dialect:process.env.DIALECT,
+//     host:process.env.DB_HOST
+// });
 
-module.exports=sequelize;
+// module.exports=sequelize;
+
+
+const { Sequelize } = require("sequelize");
+
+
+const  sequelize= new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_ROOT_PASSWORD,
+  {
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+  }
+);
+
+module.exports = sequelize;
